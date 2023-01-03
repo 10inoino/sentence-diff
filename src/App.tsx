b7cc5c9d12d316d56ccc728095322af7c72b2443
10inoino <inomatahi@icloud.com>
2023-01-03 05:23:03 +0000

    confirm diff

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import Diff from './Diff';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <p>Hello React.</p>
+      <main>
+        <h1>Letter-by-letter diff</h1>
+        <Diff string1="Kyle Peacock" string2="Kyle Matthews" />
+        <br />
+        <h2>Word diff</h2>
+        <Diff
+          string1="It is Wednesday, my dude"
+          string2="It is Saturday, my dude"
+          mode="words"
+        />
+      </main>
     </div>
   );
 }
